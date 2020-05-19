563848c9727d8a65a3952b11224acf1ff948b802
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2020-05-19 14:54:24 +0100

    explore nexus tree

--- read_example.py
+++ read_example.py
@@ -15,15 +15,16 @@ def look_up_dataset_names(nx_group):
     return dataset_names
 
 # recursively traverse tree and build tree model
-def look_up_tree(nx_group, root = ""):
+def get_tree(nx_group, parent = ""):
     dataset_names = []
+    nx_tree = {}
     for group_key in nx_group.keys():
         #stop condition
-        if type(nx_group[group_key]) != h5py._hl.group.Group:
-            dataset_names.append(nx_group[group_key].name)
+        if type(nx_group[group_key]) == h5py._hl.dataset.Dataset:
+            nx_tree[group_key] = [nx_group[group_key].name, "data"]
         elif type(nx_group[group_key]) == h5py._hl.group.Group:
-            dataset_names += look_up_tree(nx_group[group_key])
-    return dataset_names
+            nx_tree[group_key] = [get_tree(nx_group[group_key]), "group"]
+    return nx_tree
 
 filename = "C:\\Users\\scman1\\Desktop\\MantisData\\TrainingCourseData\\PG3_4871_event.nxs"
 
@@ -39,3 +40,8 @@ with h5py.File(filename, "r") as nx:
 
 for indx, name in enumerate(nx_set_names):
     print(indx, name)
+
+with h5py.File(filename, "r") as nx:
+    nx_tree = get_tree(nx, parent = "")
+    
+print(len(nx_tree))
